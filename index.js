54c9599bfb1880618171ce9436e65aca32c110cc
Jason Wortley <jason@jasonwortley.com>
2019-03-05 22:24:10 -0800

    Added comments to index.js, cleaned up unused/commented code

--- index.js
+++ index.js
@@ -7,25 +7,16 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 
 //variables
-const allItemsURI = 'https://oldschoolrunescape.fandom.com/wiki/Category:Items';
-const baseURI = 'https://oldschoolrunescape.fandom.com';
-const allItemData = require('./osrs-item-data-no-0s.min.json');
-const allItemLinkArray = require('./osrs-wiki-links-all-items.json');
-const allItemLinkArrayLength = allItemLinkArray.length;
-const anchorLinkArray = [];
+const allItemsURI = 'https://oldschoolrunescape.fandom.com/wiki/Category:Items'; // Wiki page containing links to every item's full page, where stat values are stored
+const baseURI = 'https://oldschoolrunescape.fandom.com'; // Base URI for the wiki, item names can be URI encoded and concatenated with this link to make valid, scrapeable links
+const allItemData = require('./osrs-item-data-no-0s.min.json'); // Temporary import for restructuring of data TODO: cleanup pipeline to create properly formatted JSON with a single function
+const allItemLinkArray = require('./osrs-wiki-links-all-items.json'); // JSON Array containing valid formatted links to the wiki pages for every item 
+const allItemLinkArrayLength = allItemLinkArray.length; // multi-use constant defined here for reduced performance overhead
+const anchorLinkArray = []; 
 const itemStatDataObjectArray = [];
 const indexesThatHaveBeenRequested = [];
 
-const requestArr = [
-  {
-    requestObj: {
-      uri: "https://oldschoolrunescape.fandom.com/wiki/Full_helmet",
-    },
-    keywords: ['helmet', 'sallet']
-  },
-  
-];
-
+// Object containing querySelector strings, used for retrieving stat data during execution of the collectItemDataFromLinks function
 const statTableSelectorObject = {
   
   attack: {
@@ -53,93 +44,56 @@ const statTableSelectorObject = {
   
 };
 
-function collectAnchors(paramObj, i) {
-  console.log('making request number', i + 1);
-  request(paramObj.requestObj, (err, res, body) => {
-    
-    if (err) {
-      console.log('encountered error during collectAnchors request number', i + 1, 'err:', err);
-    }
-    
-    const $ = cheerio.load(body);
-    
-    const anchorArr = [];
+// Function that takes in a starting wiki page containing 200 links to item pages (the pages have the stats we are after)we push all 
+// of those anchors into one array for future mapping. This function will recursively call itself with sequential pages until it 
+// finds a page without a 'next page' button (the last page). At that point the array is written to disk as JSON using the writeFile function
+function collectAllItemsList(URI = allItemsURI, recursionCounter = 1) {
   
-    $("a").each(function(i, link){
-      anchorArr[i] = $(link).attr("href");
-    });
-    
-    console.log('anchorArr', anchorArr);
-    
-    const keywordFiltered = [];
-    
-    anchorArr.map((currentAnchor) => {
-      return paramObj.keywords.map((e) => {
-        if (!currentAnchor || !(typeof currentAnchor)) {
-          return
-        }
-        if (currentAnchor.includes(e)) {
-          keywordFiltered.push(currentAnchor)
-        }
-      })
-    });
-    
-    const filteredHREFArr = keywordFiltered.filter(e => e.startsWith('/wiki/'));
-    
-    console.log('filteredHREFArr', filteredHREFArr);
-    
-  });
-}
-
-function collectAllItemsList(URI = allItemsURI) {
-  
-  if (!URI) {
+  if (!URI) { //error prevention
     console.log('collectAllItemsList called without URI, returning');
     return;
   }
   
-  request(URI, (err, res, body) => {
+  request(URI, (err, res, body) => { // make a web request for the page
     if (err) {
-      console.log('encountered error during collectAllItemsList, err:', err);
+      console.log('Encountered error during collectAllItemsList, err:', err);
     }
     
-    console.log('collectAllItemsList called');
+    console.log('collectAllItemsList request succeeded, parsing page data. Current page count:', recursionCounter);
   
     const $ = cheerio.load(body);
-    const table = 'div.category-page__members';
-    const nextPageAnchor = $('a.category-page__pagination-next')[0];
+    const table = 'div.category-page__members'; // querySelector for relevant element
+    const nextPageAnchor = $('a.category-page__pagination-next')[0]; // querySelector for 'next page' link, if not found recursion ends
     const nextPageLink = nextPageAnchor ? nextPageAnchor.attribs.href : null; //error handle for final page
   
+    // Load the section of the page containing desired anchors into cheerio, get all anchor links, push each link to anchorLinkArray
     $(table).find('a').each((i, e) => {anchorLinkArray.push(e.attribs.href)});
     
+    // Pages contain 200 items max, final page doesn't contain a next button and will end the recursion
     if (nextPageLink) {
-      console.log('nextPageLink exists, calling collectAllItemsList');
-      collectAllItemsList(nextPageLink);
-    }
-    else {
-      console.log('nextPageLink does not exist, writing file');
-      fs.writeFile('osrs-wiki-links-all-items.min.json', JSON.stringify(anchorLinkArray), err => {
-        if (err) {
-          console.log('error writing file', err);
-        }
-        else {
-          console.log('file written');
-        }
-      })
+      console.log('collectAllItemsList recurring for next page of anchors');
+      collectAllItemsList(nextPageLink, recursionCounter + 1);
     }
     
-  })
-}
-
-function collectItemDataFromLinks(index) {
-  console.log('CollectItemDataFromLinks called with index', index, '- allItemLinkArrayLength', allItemLinkArrayLength);
+    else {
+      console.log('nextPageLink does not exist, writing file. Final page count:', recursionCounter);
+      writeFile('osrs-wiki-links-all-items.min.json', anchorLinkArray);
+    }
+    
+  }) // end request callback
   
-  if (!allItemLinkArray[index]) {
+} // end function
+
+// 
+function collectItemDataFromLinks(index) {
+  console.log('CollectItemDataFromLinks called with index', index, '--- allItemLinkArrayLength', allItemLinkArrayLength);
+  
+  if (!allItemLinkArray[index]) { //error handling
     console.log('CollectItemDataFromLinks called without URI, returning');
     return;
   }
   
-  const URI = (baseURI + allItemLinkArray[index]);
+  const URI = (baseURI + allItemLinkArray[index]); // concatenate to make a requestable link
   
   request(URI, (err, res, body) => {
     if (err) {
@@ -148,10 +102,8 @@ function collectItemDataFromLinks(index) {
     
     const $ = cheerio.load(body);
     
-    //console.log('$', $('div.pi-smart-data-value[data-source^="astab"]'));
-    
-    const statTableSelectorKeys = Object.keys(statTableSelectorObject);
-    const itemStatObject = {
+    const statTableSelectorKeys = Object.keys(statTableSelectorObject); // makes an array of strings that are used for bracket notation lookups of that object's properties
+    const itemStatObject = { // initialize object properties for storing the stat values from the page
       attack: {
         stab:  0,
         slash: 0,
@@ -176,27 +128,29 @@ function collectItemDataFromLinks(index) {
       }
     };
   
-    let jLoopHasBroken = false;
+    let jLoopHasBroken = false; // j loop only breaks if there is an error getting stat data
     
+    // Nested loops because our querySelectors are nested within an object not really necessary but makes sense when looking at
+    // the structure of our data
     for (let i = 0; i < statTableSelectorKeys.length; i++) {
-      if (jLoopHasBroken) {
+      
+      if (jLoopHasBroken) { // Breaks if there is an error getting stat data
         break;
       }
+      
       const currentStatTableSelectorObjectKey = statTableSelectorKeys[i];
+      
+      // Creates an array of our querySelectors to loop over 
       const currentObjectSelectors = Object.keys(statTableSelectorObject[currentStatTableSelectorObjectKey]);
       
       for (let j = 0; j < currentObjectSelectors.length; j++) {
         
+        // cheerio call with current querySelector
         const currentDOMElement = $(statTableSelectorObject[currentStatTableSelectorObjectKey][currentObjectSelectors[j]]);
-        const currentStatValue = currentDOMElement.text();
+        const currentStatValue = currentDOMElement.text(); // current data we're after
         
-        // console.log('statTableSelectorObject[currentStatTableSelectorObjectKey][currentObjectSelectors[j]]',
-        //   statTableSelectorObject[currentStatTableSelectorObjectKey][currentObjectSelectors[j]]);
-        
-        console.log('CurrentStatValue', currentStatValue);
-        
-        if (!currentStatValue) {
-          console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Breaking out of loops with index', index);
+        if (!currentStatValue) { //error handling
+          console.log('Breaking out of loops with index', index);
           shouldWriteToFileOrRecurse(index);
           jLoopHasBroken = true;
           break;
@@ -277,32 +231,32 @@ function removeItemsWithoutStats() {
   writeFile('osrs-item-data-truly-no-0s.min.json', itemsWithData);
 }
 
-app.get('/collectAnchors', (req, res) => {
-  console.log('requesting all links in requestArr');
-  requestArr.forEach(collectAnchors);
-  res.send('requesting all links in requestArr');
+app.get('/' (req, res) => {
+  res.send('Available links are /getPartialURIForEveryItem, /getItemDataFromLinks, /removeItemsWithoutStats');
+})
+
+app.get('/getPartialURIForEveryItem', (req, res) => {
+  console.log('Collecting all items in list');
+  
+  collectAllItemsList(); // gets a partial link for every item in the game and stores it on the disk as a JSON array 
+  
+  res.send('Collecting all partial URIs from wiki pages');
 });
 
-app.get('/collectAllItemsList', (req, res) => {
-  console.log('collecting all items in list');
-  collectAllItemsList();
-  res.send('collecting all items in list');
-});
-
-app.get('/collectItemDataFromLinks', (req, res) => {
-  console.log('collecting all item data from links');
-  // let index = 0;
-  // function incremementIndex() {
-  //   index++;
-  // }
+app.get('/getItemDataFromLinks', (req, res) => {
+  console.log('Collecting all item data from links');
+  
   collectItemDataFromLinks(0);
   indexesThatHaveBeenRequested.push(0);
-  res.send('collecting all item data from links');
+  
+  res.send('Collecting all item data from links');
 });
 
 app.get('/removeItemsWithoutStats', (req, res) => {
+  
   removeItemsWithoutStats();
-  res.send('doing stuff')
+  
+  res.send('Removing item data that contains only values of 0')
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
